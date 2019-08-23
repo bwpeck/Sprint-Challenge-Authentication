45fabea1acfdf357bb95f21ed9e4136b1b887556
bwpeck <benjaminwade483@yahoo.com>
2019-08-23 17:45:25 -0400

    auth-router-logout done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,37 +5,53 @@ const Users = require('../users/users-model.js');
 const restricted = require('./authenticate-middleware');
 
 router.post('/register', (req, res) => {
-    let user = req.body;
+  let user = req.body;
 
-    if (!user.username || !user.password) {
-        res.status(404).json({ message: 'No username or password submitted.' });
-    }
-    const hash = bcrypt.hashSync(user.password, 12);
-    user.password = hash;
-    Users.add(user)
-        .then((saved) => {
-            res.status(201).json(saved);
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        });
+  if (!user.username || !user.password) {
+    res.status(404).json({ message: 'No username or password submitted.' });
+  }
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json(saved);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
-      .first()
-      .then((user) => {
-          console.log(user);
-          if (username && bcrypt.compareSync(password, user.password)) {
-              req.session.user = user;
-              res.status(201).json({ message: 'Logged in' });
-          } else {
-              res.status(404).json({ message: 'You shall not pass!' });
-          }
-      })
-      .catch((err) => {
-          res.status(500).json(err);
-      });
+    .first()
+    .then((user) => {
+      console.log(user);
+      if (username && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
+        res.status(201).json({ message: 'Logged in' });
+      } else {
+        res.status(404).json({ message: 'You shall not pass!' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
+
+router.get('/logout', restricted, (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ message: 'There was an error' });
+      }
+      res.end();
+    });
+  } else {
+    res.end();
+  }
+});
+
+module.exports = router;
